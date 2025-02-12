6c54a9c1420e78af7c38ea3bfce9401a6fce1377
Raghuveer Draksharam <42346842+raghuveer88@users.noreply.github.com>
2025-02-12 12:34:17 -0500

    Time: 3 ms (37.75%), Space: 21.2 MB (26.17%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -5,21 +5,18 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    # In the problem he gave that it is a binary tree. so if we do inorder traversing then
-    # we get a sorted array. once we get try to return the kth smallest number 
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        res_arr = []
-        res_arr = self.dsf(root, res_arr)
-        # res_arr.sort()
-        return res_arr[k-1]
+        res = []
+        def inorder(root):
+            if root:
+                inorder(root.left)
+                res.append(root.val)
+                inorder(root.right)
 
-    def dsf(self, root: Optional[TreeNode], res_arr):
-        if not root:
-            return 
-        self.dsf(root.left, res_arr)
-        res_arr.append(root.val)
-        self.dsf(root.right, res_arr)
-        return res_arr
-        
+        inorder(root)
 
-        
+        if res:
+            return res[k-1]
+        else:
+            return None
+    
