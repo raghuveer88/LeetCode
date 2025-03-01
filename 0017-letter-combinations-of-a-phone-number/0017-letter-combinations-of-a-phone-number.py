09095aff52f9f9c03100950b463ef9d16606a926
Raghuveer Draksharam <42346842+raghuveer88@users.noreply.github.com>
2025-02-28 19:59:25 -0500

    Time: 3 ms (5.31%), Space: 17.7 MB (68.78%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,35 +1,25 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        # It maps each digit (from '2' to '9') to its corresponding letters,
-        #  then uses a recursive function to build combinations by exploring
-        #  each letter option for the current digit. If a combination matches 
-        # the length of the input digits, it’s added to the result list. 
-        # The recursion continues until all possible combinations are 
-        # explored, returning the list of results.
-        
-        
         res = []
-        map_nums_digit = {
-            "2":"abc",
-            "3":"def",
-            "4":"ghi",
-            "5":"jkl",
-            "6":"mno",
-            "7":"pqrs",
-            "8":"tuv",
-            "9":"wxyz"
+        digittostr = {
+            "2" : "abc",
+            '3' : "def",
+            "4" : "ghi",
+            "5" : "jkl",
+            "6" : "mno",
+            "7" : "pqrs",
+            "8" : "tuv",
+            "9" : "wxyz"
         }
 
-        def backtracking(i,currStr):
-            if len(currStr) == len(digits):
-                res.append(currStr)
-                return
-            
-            for c in map_nums_digit[digits[i]]:
-                backtracking(i+1,currStr + c)
+        def backtracking(i, currstr):
+            if len(currstr) == len(digits):
+                res.append(currstr)
+                return 
+            for c in digittostr[digits[i]]:
+                backtracking(i+1,currstr + c)
 
-        
         if digits:
             backtracking(0,"")
-        
+
         return res
