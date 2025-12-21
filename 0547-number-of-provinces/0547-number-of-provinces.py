5b497799b5939269014059810beecd42d729e540
Raghuveer Draksharam <42346842+raghuveer88@users.noreply.github.com>
2025-12-20 19:04:24 -0500

    Time: 4 ms (71.8%), Space: 18.5 MB (98.61%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,28 +1,18 @@
-class Solution(object):
-    def findCircleNum(self, isConnected):
-        """
-        :type isConnected: List[List[int]]
-        :rtype: int
-        """
-        # You maintain a visited set in which you add all the visited nodes
-        # first go through the first node and find all the attachments or paths to it
-        # if a path exist to another node now go to that node add it to vistited set 
-        # and try to find all the paths to it.
-        # if a new node is there which is not visited then you need to increase the 
-        # provinces
-
-        def dfs(start):
-            visited.add(start)
-            for neighbor in range(len(isConnected)):
-                if isConnected[start][neighbor] and neighbor not in visited:
-                    dfs(neighbor)
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
         
-        visited = set()
-        provinces = 0
-        for start in range(len(isConnected)):
-            if start not in visited:
-                provinces += 1
-                dfs(start)
+        total_nodes = len(isConnected)
+        visited = [False]*total_nodes
 
+        def dsf(city):
+            visited[city] = True
+            for j in range(total_nodes):
+                if not visited[j] and isConnected[city][j] == 1:
+                    dsf(j)
+        
+        provinces = 0
+        for i in range(total_nodes):
+            if not visited[i]:
+                provinces += 1
+                dsf(i)
         return provinces
-            
