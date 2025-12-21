20b3849a5804c9dca855b0bfb86f8be15fc4aee0
Raghuveer Draksharam <42346842+raghuveer88@users.noreply.github.com>
2025-12-21 01:20:39 -0500

    Time: 76 ms (62.67%), Space: 19.7 MB (57.62%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -1,28 +1,32 @@
-class Solution(object):
-    def nearestExit(self, maze, entrance):
-        """
-        :type maze: List[List[str]]
-        :type entrance: List[int]
-        :rtype: int
-        """
-        # This problem we can solve using BSF so each time you go to a point add all the possible next
-        # steps which is up,down,left and right to the queue and now check from those points 
-        #  if you are the last row or last col then you can simply exist and return the steps
-        #  if you are not then just pop and move to next steps like a repetative process
+class Solution:
+    def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
+        rows, cols = len(maze),len(maze[0])
+        sr,sc= entrance[0],entrance[1]
+        step = 0
+        q = deque([(sr,sc,step)])
 
-        steps = 0
-        q = deque([(entrance[0],entrance[1],steps)])
-        possible_moves = [(-1,0),(1,0),(0,1),(0,-1)]
+        direction = [(1,0),(-1,0),(0,-1),(0,1)]
+        visited = set([(sr,sc)])
 
-        visited = set()
-        visited.add((entrance[0], entrance[1]))
         while q:
-            x,y,steps = q.popleft()
-            if (x == len(maze)-1 or y == len(maze[0])-1 or x == 0 or y == 0) and [x, y] != entrance:
-                return steps
-            else:
-                for i,j in possible_moves:
-                    if 0<= i+x < len(maze) and 0<= j+y < len(maze[0]) and maze[i+x][j+y] == '.' and (i+x,j+y) not in visited:
-                        q.append((i+x,j+y,steps+1))
-                        visited.add((i+x,j+y))
+            r,c,step = q.popleft()
+            
+            for dr,dc in direction:
+                ur,uc = r+dr,c+dc
+                if ur<0 or ur>=rows or uc<0 or uc>=cols:
+                    continue
+                if maze[ur][uc] != '.':
+                    continue
+                
+                if (ur,uc) in visited:
+                    continue
+
+                if ur == rows-1 or ur == 0 or uc == cols-1 or uc == 0:
+                    return step + 1
+                visited.add((ur,uc))
+                q.append((ur,uc,step+1))
+                
         return -1
+
+
+
