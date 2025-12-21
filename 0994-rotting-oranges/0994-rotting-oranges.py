91ced7bb0cf709c38611b2f0625c70be2635d8d0
Raghuveer Draksharam <42346842+raghuveer88@users.noreply.github.com>
2025-12-21 17:13:27 -0500

    Time: 7 ms (24.18%), Space: 17.3 MB (98.17%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,39 +1,41 @@
-class Solution(object):
-    def orangesRotting(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
-        # In this problem firstly calculate the number of fresh organges
-        # and append all the rotten oranges in the queue
-        # and the 4 directions positions are mentioned in the possible_moves which is 
-        # up, down, left and right. Now just do the 4 operation for all the rotten oranges
-        # in the queue and as soon a fresh_orange is turned rotten reduce the fresh orange count
-        # once all the fresh oranges are 0 then return the minutes else return -1
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
         q = deque()
-        fresh_oranges = 0
-        minutes = 0
-        zero_count = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
+        fresh = 0
+        minutes = -1
+        
+        for i in range(rows):
+            for j in range(cols):
                 if grid[i][j] == 2:
-                    q.append((i,j,minutes))
+                    q.append((i,j))
                 if grid[i][j] == 1:
-                    fresh_oranges += 1
-                
+                    fresh += 1
+        
+        if fresh == 0:
+            return 0
 
-        possible_moves = [(1,0),(-1,0),(0,1),(0,-1)]
-
+        directions = [(1,0),(-1,0),(0,1),(0,-1)]
         while q:
-            x,y, minutes = q.popleft()
-            for i,j in possible_moves:
-                if 0<=i+x<len(grid) and 0<=j+y<len(grid[0]) and grid[i+x][j+y] == 1:
-                    grid[i+x][j+y] = 2
-                    q.append((i+x,j+y,minutes+1))
-                    fresh_oranges -= 1
-        if fresh_oranges == 0:
-                return minutes
+
+            for i in range(len(q)):
+                r,c = q.popleft()
+
+                for dr,dc in directions:
+                    nr,nc = r+dr,c+dc
+
+                    if nr >=0 and nr < rows and nc >=0 and nc < cols:
+                        if grid[nr][nc] == 1:
+                            fresh -= 1
+                            grid[nr][nc] = 2
+                            q.append((nr,nc))
+            
+            minutes += 1
+
+        
+        if fresh == 0:
+            return minutes
         else:
             return -1
-        
-        
+                    
