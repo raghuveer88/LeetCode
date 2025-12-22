3251a78858f3605fbaed81ca3512995370f22b4d
Raghuveer Draksharam <42346842+raghuveer88@users.noreply.github.com>
2025-12-22 13:23:03 -0500

    Time: 739 ms (16.88%), Space: 43.5 MB (10.96%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
@@ -1,22 +1,23 @@
 class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
-        # quite simple you just need to sort potions and do bnary search to get the elment 
-        # which is the first element when multiplied with spell is greater than success
-        # so all the other elements to the right will automatically be greater 
-        
-        potions = sorted(potions)
-        count = []
-        
-        for i in spells:
-            l,r = 0,len(potions)-1
-            idx_val = len(potions)
-            while l <= r:
-                m = (l + r) //2
-                if i * potions[m] >= success:
-                    r = m -1
-                    idx_val = m
+        ans = []
+        n = len(spells)
+        m = len(potions)
+        potions.sort()
+        for i in range(n):
+            count = 0
+            temp = 0
+            low, high = 0, m-1
+            while low <= high:
+                mid = (low + high)//2
+                if spells[i]*potions[mid]>= success:
+                    high = mid -1
+                    temp = mid
+                    count = m -temp
                 else:
-                    l = m +1
-            count.append(len(potions) - idx_val)
+                    low = mid+1
+                
+            ans.append(count)
 
-        return count
+        return ans
+            
