bfc9f77c1e1f9588e98b848d08a43f5b88a85219
Raghuveer Draksharam <42346842+raghuveer88@users.noreply.github.com>
2025-12-22 22:32:47 -0500

    Time: 0 ms (100%), Space: 17.2 MB (98.62%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,17 +1,23 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        res = []
+        
+        path = []
+        ans = []
 
-        def dsf(curr,n,k,start):
-            if n==0 and k == 0:
-                res.append(curr)
+        def backtrack(start,k_left,remain):
+            if k_left == 0:
+                if remain == 0:
+                    ans.append(path[:])
                 return
-            if k <=0:
+            if remain < 0:
                 return
 
-            for i in range(start,min(n+1,10)):
-                dsf(curr + [i], n-i, k-1,i + 1)
-            return
-        dsf([],n,k,1)
+            for x in range(start,10):
+                if x > remain:
+                    break
+                path.append(x)
+                backtrack(x+1,k_left-1,remain-x)
+                path.pop()
 
-        return res
+        backtrack(1,k,n)
+        return ans
