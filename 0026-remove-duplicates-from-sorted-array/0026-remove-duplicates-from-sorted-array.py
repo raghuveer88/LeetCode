be91cab8ce6284e627a1d60c0cd2f575cb254d92
Raghuveer Draksharam <42346842+raghuveer88@users.noreply.github.com>
2025-12-25 18:15:38 -0500

    Time: 3 ms (47.93%), Space: 18.1 MB (99.97%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,13 +1,13 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        # since the array is already in acending order. evertime you enconter a new 
-        # number for the first time asiign it to jth index which is responsiable to 
-        #change have the unique elements from beginning.
+        
+        prev = 0
 
-        j = 1
-        for i in range(1,len(nums)):
-            if nums[i] != nums[i-1]:
-                nums[j] = nums[i]
-                j += 1
+        for i in range(len(nums)):
+            if nums[prev] == nums[i]:
+                continue
+            else:
+                nums[prev+1] = nums[i]
+                prev += 1
 
-        return j
+        return prev+1        
