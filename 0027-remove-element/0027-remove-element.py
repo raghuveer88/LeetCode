2afb9cb30c0047ba9933da284a2a78ec9e6c20a1
Raghuveer Draksharam <42346842+raghuveer88@users.noreply.github.com>
2025-12-25 17:56:10 -0500

    Time: 0 ms (100%), Space: 17.3 MB (92.72%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,26 +1,15 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        # In this problem we are solving it using two pointer. I think what I have noticed is 
-        # when using two pointer approach you need to use while loop with a condition including
-        # pointers and not the array size. so for this sum took a start point and the end point
-        # if I see a nums[start] with a val just swap it to the end and decrement the end by 1 
-        # but don't increament the start as the swapped end might still be a value with val so needs
-        # to be checked again.
-
-
-        end = len(nums) - 1
-        start = 0
-
-        while start <= end:
-            if nums[start] == val:
-                nums[start] = nums[end]
-                nums[end] = val
-                end = end -1
-
-            else:
-                start = start + 1
-
-        return end+1
-
         
-            
+        i = 0
+        j = len(nums)-1
+
+        while i<=j:
+            if nums[i] == val:
+                nums[i], nums[j] = nums[j],nums[i]
+                j -= 1
+            else:
+                i = i+ 1
+
+        return i
+
