c69c779392f0dc9547f0fc109e52cb2fddf994cf
Raghuveer Draksharam <42346842+raghuveer88@users.noreply.github.com>
2025-12-26 02:45:31 -0500

    Time: 97 ms (48.67%), Space: 26.3 MB (95.26%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,17 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # this uses a sliding window technique I took two pointers l and r then if the price of
-        # l is less than r then update l to r. the goal is to find the a sum or difference which 
-        #is greater than any pair we saw before.
-        total = 0
-        l = 0
-        for r in range(1,len(prices)):
-            if prices[l] > prices[r]:
-                l = r
-            else:
-                sum = prices[r] - prices[l]
-                if sum > total:
-                    total = sum
-                    
-                
-        return total
+
+
+        lowest = prices[0]
+        res = 0
+
+        for i in range(len(prices)):
+            if prices[i] < lowest:
+                lowest = prices[i]
+            
+            res = max(res,prices[i]-lowest)
+
+        return res
+
+            
