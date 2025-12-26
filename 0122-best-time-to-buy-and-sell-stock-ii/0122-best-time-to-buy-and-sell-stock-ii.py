f76d2f801ec9285731523b91a8025a8dce1fa1d3
Raghuveer Draksharam <42346842+raghuveer88@users.noreply.github.com>
2025-12-26 15:11:22 -0500

    Time: 4 ms (19.54%), Space: 18.1 MB (94.27%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,15 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # so firstly you can see this like a graph of prices. what we need to findout is everytime
-        # there is a up from the down you add it to the final sum. at the end return the sum.
-        l = 0
-        sum = 0
-        for r in range(1,len(prices)):
-            if prices[l] > prices[r]:
-                l = l+1
-            else:
-                sum = sum + prices[r] - prices[l]
-                l = l+1
-        
-        return sum
-            
+        cash = 0
+        hold = cash-prices[0]
+
+        for p in prices:
+            cash = max(cash,hold+p)
+            hold = max(hold,cash-p)
+
+        return cash
