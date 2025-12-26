4fb14c93073ad0c912644362923d1ab2e26a3b1b
Raghuveer Draksharam <42346842+raghuveer88@users.noreply.github.com>
2025-12-26 15:28:45 -0500

    Time: 31 ms (55.17%), Space: 18.6 MB (42.73%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,14 +1,31 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        # first keep max_index to where the i+nums[i] can go max. so which means now unitl that
-        #max_index you can access any element you want. By any chance if the for look index 'i'
-        # is greater than max_index which means you are not able to access that index return false.
-        max_index = 0
-        for i in range(len(nums)):
-            if i > max_index:
-                return False
-            else:
-                temp = i + nums[i]
-                max_index = max(max_index, temp)
+        # goal = len(nums)-1
+        # j = 0
+
+        # if goal == 0:
+        #     return True
+
+        # for i in range(len(nums)-1):
+        #     j = i + nums[i]
+        #     if j >= goal:
+        #         return True
+
             
-        return True
+        
+        # return False
+
+        goal = len(nums)-1
+        farest = 0
+        i = 0
+
+        while i<=farest:
+            farest = max(farest,i+nums[i])
+
+            if farest >= goal:
+                return True
+            i = i+1
+
+        
+        return False
+
