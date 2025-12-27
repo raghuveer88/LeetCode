bda0d5dccb37d28f45ee85dd9e0ad8095a248bf3
Raghuveer Draksharam <42346842+raghuveer88@users.noreply.github.com>
2025-12-27 18:35:15 -0500

    Time: 8 ms (27.82%), Space: 17.2 MB (95.59%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,29 +1,24 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        # This code is self explanatory just go throught the code it has got if statements
-        # which handles the logic.
-        res = 0
-        mydict = {
-            "I" : 1,
-            "V" : 5,
-            "X" : 10,
-            "L" : 50,
-            "C" : 100,
-            "D" : 500,
-            "M" : 1000
+        
+        symbols = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
         }
+        res = 0
         i = 0
         while i < len(s):
-            if i + 1 < len(s) and (
-                (s[i] == "I" and s[i+1] in {"V", "X"}) or 
-                (s[i] == "X" and s[i+1] in {"L", "C"}) or 
-                (s[i] == "C" and s[i+1] in {"D", "M"})
-            ):
-                    res = res + mydict[s[i+1]] - mydict[s[i]]
-                    i = i + 2
-                    continue
+            if i+1 < len(s) and symbols[s[i+1]] > symbols[s[i]]:
+                res = res -symbols[s[i]]
             else:
-                res = res + mydict[s[i]]
-            i = i + 1
+                res = res + symbols[s[i]]
+            i = i+1
             
+
         return res
+                
