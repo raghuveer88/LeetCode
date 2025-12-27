9b3a1c795dd29d0a3aeaca0675c4e400c0e44287
Raghuveer Draksharam <42346842+raghuveer88@users.noreply.github.com>
2025-12-27 00:33:49 -0500

    Time: 43 ms (12.2%), Space: 25.7 MB (31.91%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,22 +1,29 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n = len(nums)
         
-        # Initialize prefix and suffix arrays
-        pre = [1] * n
-        suf = [1] * n
+        left = [nums[0]]
+        right = list(reversed(nums))
+        ans = []
+
+        for i in range(1,len(nums)):
+            left.append(left[i-1]*nums[i])
+
+        for i in range(1,len(right)):
+            right[i] = right[i-1]*right[i]
+
+        right.reverse()
+
+        for i in range(len(left)):
+            if i-1 >=0 and i+1<len(left):
+                ans.append(left[i-1]*right[i+1])
+            elif i-1<0:
+                ans.append(right[i+1])
+            elif i+1 >= len(left):
+                ans.append(left[i-1])
+
+        return ans
+
+# 1,2,6,12
+
+# 24,24,12,4        
         
-        # Compute prefix product
-        for i in range(1, n):
-            pre[i] = pre[i - 1] * nums[i - 1]
-        
-        # Compute suffix product
-        for i in range(n - 2, -1, -1):
-            suf[i] = suf[i + 1] * nums[i + 1]
-        
-        # Compute result using prefix and suffix
-        res = []
-        for i in range(n):
-            res.append(pre[i] * suf[i])
-        
-        return res
