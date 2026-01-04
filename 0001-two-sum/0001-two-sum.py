8e84894c665c50dc238327b4fabe0980b0e00c01
Raghuveer Draksharam <42346842+raghuveer88@users.noreply.github.com>
2026-01-03 20:52:36 -0500

    Time: 1 ms (57.12%), Space: 18.9 MB (26.11%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # For this use hash map first check the difference between the target and the 
-        # element then check if the differece number is in the map. If not just add the 
-        # element and it's index to the map.
-        map = {}
-        i = 0
-        while i< len(nums):
-            dif = target - nums[i]
-            if dif in map:
-                return [map[dif], i]
-            map[nums[i]] = i
-            i = i + 1
         
+        map = {}
+
+        for i in range(len(nums)):
+            if nums[i] in map:
+                return [map[nums[i]], i]
+            else:
+                diff = target-nums[i]
+                map[diff] = i
+            
