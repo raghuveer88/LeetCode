bfc3a379f32152761ac83a5ef791aeb543745e9b
Raghuveer Draksharam <42346842+raghuveer88@users.noreply.github.com>
2026-01-06 07:41:12 +0530

    Time: 0 ms (100%), Space: 17.4 MB (80.33%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,37 +5,32 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        #This question is also pretty straight forward you just need travers the linked
-        # list then put it in the array then reverse it then add it then put the 
-        # new summend value in an array and reverse it and make it to a linked list
-        l1_arr = []
-        temp = l1
-        while temp:  # Fixed: Include last node
-            l1_arr.append(temp.val)
-            temp = temp.next
+
+        head = ListNode(0)
+        tail = head
+        carry = 0
+        while l1 or l2 or carry:
+            tail.next = ListNode()
+            tail = tail.next
+            if l1 and l2:
+                sum = l1.val + l2.val + carry
+                l1 = l1.next
+                l2 = l2.next
+            elif l1:
+                sum = l1.val + carry
+                l1 = l1.next
+            elif l2:
+                sum = l2.val + carry
+                l2 = l2.next
+            elif carry:
+                sum = carry
+            tail.val = sum%10
+            carry = sum//10
+
+            
         
-        l2_arr = []
-        temp2 = l2
-        while temp2:  # Fixed: Include last node
-            l2_arr.append(temp2.val)
-            temp2 = temp2.next
+        return head.next
 
-        # Reverse and convert to integers
-        l1_arr.reverse()
-        l2_arr.reverse()
+            
 
-        l1_int = int("".join(map(str, l1_arr)))  # Fixed: Removed redundant list comprehension
-        l2_int = int("".join(map(str, l2_arr)))
 
-        res = l1_int + l2_int
-        res = [int(digit) for digit in str(res)]
-        res.reverse()
-
-        # Create new linked list
-        head = ListNode(res[0])  # Fixed: Removed `l3.head`
-        temp3 = head
-        for i in res[1:]:
-            temp3.next = ListNode(i)
-            temp3 = temp3.next
-        
-        return head 
