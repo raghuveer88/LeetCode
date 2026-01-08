7af23e18eab0204bd4b82bf1325955ed47598e27
Raghuveer Draksharam <42346842+raghuveer88@users.noreply.github.com>
2026-01-08 17:14:52 +0530

    Time: 0 ms (100%), Space: 19.3 MB (14.19%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -7,41 +7,24 @@
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
 
-        if not p and not q:
-            return True
-        if not p or not q:
-            return False
-
-        q1 = deque([(p,"l")])
-        q2 = deque([(q,"l")])
-
-        while q1 and q2:
-
-            for i in range(len(q2)):
-                item1, side1 = q1.popleft()
-                item2, side2 = q2.popleft()
-
-                if not item1 and not item2:
-                    continue
-                if not item1 or not item2:
-                    return False
-
-                if item1.val != item2.val or side1 != side2:
-                    return False
-
-                
-                q1.append((item1.left,"l"))
-                
-                q2.append((item2.left,"l"))
-                
-                q1.append((item1.right,"r"))
-                
-                q2.append((item2.right,"r"))
+        def rec(p,q):
+            if not p and not q:
+                return True
             
-        if len(q1) != 0 or len(q2) != 0:
-            return False
+            if not p or not q:
+                return False
+
+            if p.val != q.val:
+                return False
+            
+            a = rec(p.left,q.left)
+            b = rec(p.right,q.right)
+
+            return a and b
         
-        return True
+        return rec(p,q)
+
+        
 
 
 
