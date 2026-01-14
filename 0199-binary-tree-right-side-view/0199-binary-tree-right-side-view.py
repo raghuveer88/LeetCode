eb8c0a3c8201ef7b44ae4f401eca9a2902e75945
Raghuveer Draksharam <42346842+raghuveer88@users.noreply.github.com>
2026-01-14 16:09:14 +0530

    Time: 0 ms (100%), Space: 19.5 MB (8.82%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -1,35 +1,27 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def rightSideView(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[int]
-        """
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        output = []
+        if not root:
+            return []
+        def rec(level):
+            if not level:
+                return
 
-        # it is bsf use the queue to solve the problem
-        # go level by level and append the last node to the result
-        
-        res = []
+            next_level = []
 
-        q = deque()
-        q.append(root)
-        
-        while q:
-            temp = []
-            for i in range(len(q)):
-                node = q.popleft()
-                if node:
-                    if node.left:
-                        q.append(node.left)
-                    if node.right:
-                        q.append(node.right)
-                    temp.append(node.val)
-            if temp:
-                res.append(temp[-1])
-        return res
+            for node in level:
+                if node.left: next_level.append(node.left)
+                if node.right: next_level.append(node.right)
             
+            output.append(level[-1].val)
+
+            rec(next_level)
+        
+        rec([root])
+        return output
