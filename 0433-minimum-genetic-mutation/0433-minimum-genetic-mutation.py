28bb5ee9ca7dfc51a7b0064c635ee87cd9781041
Raghuveer Draksharam <42346842+raghuveer88@users.noreply.github.com>
2026-01-29 17:31:41 +0530

    Time: 4 ms (2.78%), Space: 19.6 MB (17.51%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
+        
+        q = deque()
+        q.append((startGene,0))
+        visited = set()
+        chars = ['A','C','G','T']
+        while q:
+            gene, move = q.popleft()
+            if gene == endGene:
+                return move
+            for i in range(8):
+                
+                for j in range(len(chars)):
+                    if gene[i] != chars[j]:
+                        term = gene[:i] + chars[j] + gene[i + 1:]
+                        if term in bank and term not in visited:
+                            q.append((term,move+1))
+                        visited.add(term)
+        return -1
+
