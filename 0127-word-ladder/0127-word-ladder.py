2bd2798621de2ed2bf76986d9de556292d32782c
Raghuveer Draksharam <42346842+raghuveer88@users.noreply.github.com>
2026-01-29 18:27:29 +0530

    Time: 7173 ms (5.35%), Space: 20.1 MB (68.17%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,6 +1,6 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        
+        # there is improved way to code this in the below comments
         q = deque()
         q.append((beginWord,1))
         visited = set()
@@ -24,4 +24,40 @@ class Solution:
                     visited.add(wordList[i])
         return 0
                 
+# from typing import List
+# from collections import defaultdict, deque
+
+# class Solution:
+#     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+#         if endWord not in wordList:
+#             return 0
+
+#         L = len(beginWord)
+#         all_words = wordList[:]  # copy
+#         all_words.append(beginWord)
+
+#         patterns = defaultdict(list)
+#         for w in all_words:
+#             for i in range(L):
+#                 pat = w[:i] + "*" + w[i+1:]
+#                 patterns[pat].append(w)
+
+#         q = deque([(beginWord, 1)])
+#         visited = {beginWord}
+
+#         while q:
+#             word, dist = q.popleft()
+#             if word == endWord:
+#                 return dist
+
+#             for i in range(L):
+#                 pat = word[:i] + "*" + word[i+1:]
+#                 for nxt in patterns[pat]:
+#                     if nxt not in visited:
+#                         visited.add(nxt)
+#                         q.append((nxt, dist + 1))
+#                 patterns[pat] = []  # key optimization
+
+#         return 0
+
 
