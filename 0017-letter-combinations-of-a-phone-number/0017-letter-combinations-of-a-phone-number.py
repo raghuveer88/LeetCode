f07c6f64919668effa282dd3ab612f796afc330f
Raghuveer Draksharam <42346842+raghuveer88@users.noreply.github.com>
2026-01-30 17:54:06 +0530

    Time: 0 ms (100%), Space: 19.5 MB (18.21%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,25 +1,28 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        res = []
-        digittostr = {
-            "2" : "abc",
-            '3' : "def",
-            "4" : "ghi",
-            "5" : "jkl",
-            "6" : "mno",
-            "7" : "pqrs",
-            "8" : "tuv",
-            "9" : "wxyz"
+        
+        mapper = {
+            '2':'abc',
+            '3':'def',
+            '4':'ghi',
+            '5':'jkl',
+            '6':'mno',
+            '7':'pqrs',
+            '8':'tuv',
+            '9':'wxyz',
         }
-
-        def backtracking(i, currstr):
-            if len(currstr) == len(digits):
-                res.append(currstr)
-                return 
-            for c in digittostr[digits[i]]:
-                backtracking(i+1,currstr + c)
-
-        if digits:
-            backtracking(0,"")
-
+        res = []
+        path = []
+        def rec(path,i):
+            if len(path)== len(digits):
+                res.append("".join(path[:]))
+                return
+            
+            
+            for v in mapper[digits[i]]:
+                path.append(v)
+                rec(path,i+1)
+                path.pop()
+        
+        rec([],0)
         return res
