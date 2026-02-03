025ea5562b100e4f84ede4cde9f8cdb37519d0d6
Raghuveer Draksharam <42346842+raghuveer88@users.noreply.github.com>
2026-02-03 17:53:26 +0530

    Time: 0 ms (100%), Space: 19.7 MB (5.52%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,16 +1,13 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         
-        k = 0
-        for i in range(len(nums)):
-            if i -1 >= 0:
-                if nums[i-1] > nums[i]:
-                    k = i
-                    nums = nums[k:] + nums[0:k]
-                    break
-
-
-        
+        # k = 0
+        # for i in range(len(nums)):
+        #     if i -1 >= 0:
+        #         if nums[i-1] > nums[i]:
+        #             k = i
+        #             nums = nums[k:] + nums[0:k]
+        #             break
 
 
         def bst(l,r):
@@ -20,11 +17,21 @@ class Solution:
             mid = (l+r)//2
 
             if nums[mid] == target:
-                return (mid + k)% len(nums)
+                return mid
             
-            elif nums[mid] > target:
-                return bst(l,mid-1)
-            elif nums[mid] < target:
-                return bst(mid+1,r)
+            if nums[l] <= nums[mid]:
+                
+                if nums[l] <=target< nums[mid]: 
+                    return bst(l,mid-1)
+                else:
+                    return bst(mid+1,r)
+            else:
+                if nums[mid] < target <= nums[r]:
+                    return bst(mid+1,r)
+                else:
+                    return bst(l,mid-1)
+
+
+            
             
         return bst(0,len(nums)-1)
