4b21f5681132cdfb34b81a24ce3f4d6f24247305
Raghuveer Draksharam <42346842+raghuveer88@users.noreply.github.com>
2026-02-06 02:39:05 +0530

    Time: 0 ms (100%), Space: 19.7 MB (12.59%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -2,4 +2,20 @@ class Solution:
     def myPow(self, x: float, n: int) -> float:
         
         
-        return math.pow(x,n)
+        def rec(a,b):
+            if b ==0:
+                return 1
+            if b == 1:
+                return a
+            if b == -1:
+                return 1/a
+            if b%2 != 0:
+                half = rec(a,b//2)
+                res = a*half*half
+                return res
+            else:
+                half = rec(a,b//2)
+                res = half*half
+                return res
+
+        return rec(x,n)
