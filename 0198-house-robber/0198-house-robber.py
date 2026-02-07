11abcaf2aefe187dbc81e07c592835dd7e335be2
Raghuveer Draksharam <42346842+raghuveer88@users.noreply.github.com>
2026-02-07 06:30:39 +0530

    Time: 0 ms (100%), Space: 19.5 MB (14.56%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,21 +1,12 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-    # for this problem just First create an array to store how much can you rob until a 
-    # certain point then for every new value in the nums array check if the last robbed value
-    # is more or the sum of the current house 'i' and the robbed house of i-2 is great
-        memo = {}
-        n = len(nums)
-
+        dp = {}
         def rec(i):
-            if i >=n:
+            if i in dp:
+                return dp[i]
+            if i >=len(nums):
                 return 0
-
-            if i in memo:
-                return memo[i]
-
-            else:
-                memo[i] = max(nums[i]+ rec(i+2), rec(i+1))
-                return memo[i]
-
+            dp[i] = max(rec(i+1),nums[i]+rec(i+2))
+            return dp[i]
 
         return rec(0)
